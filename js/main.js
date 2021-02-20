596a62ffde4b7a089f2d9345205debc0e4ebb958
EURosales <eduardo_rosales26@outlook.com>
2021-02-19 21:29:26 -0600

    1.Card design ended. 2.Links were added to the buttons. 3.General design fixes. 4.Responsive capabilities for info-categories-cards were added.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//nav scroll animation
 const header = document.querySelector(".main-header");
 window.addEventListener("scroll", () => {
   const scrollPos = window.scrollY;
@@ -7,3 +8,19 @@ window.addEventListener("scroll", () => {
     header.classList.remove("scrolled");
   }
 });
+//mail button
+const insta_btn = Array.from(document.getElementsByClassName("insta-btn"));
+insta_btn.forEach(page);
+function page(element) {
+  element.addEventListener("click", () => {
+    location.href = "https://www.instagram.com/ferman_electronics/";
+  });
+}
+//mail button
+const cot_btn = Array.from(document.getElementsByClassName("cot-btn"));
+cot_btn.forEach(mail);
+function mail(element) {
+  element.addEventListener("click", () => {
+    location.href = "mailto:electronics.ferman.ventas@gmail.com";
+  });
+}
